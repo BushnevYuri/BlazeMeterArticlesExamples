14f0712221c95f9483e8cd3e2802652d027a586b
Yuri Bushnev <ybushnev@alpha-sense.com>
2017-11-05 16:24:39 +0200

    [Powerful assertions with locust] - locust response body xpath assertion example

--- Powerfull assertions with Locust/PowerfullAssertionsWithLocust.py
+++ Powerfull assertions with Locust/PowerfullAssertionsWithLocust.py
@@ -1,14 +1,23 @@
 import re
 from locust import HttpLocust, TaskSet, task
+from lxml import html
 
 class BrowseDocumentation(TaskSet):
     HOME_PAGE_TITLE_REGEX = re.compile(".*Welcome to the Simple Travel Agency!.*")
+    HOME_PAGE_TITLE_XPATH = "//div[contains(@class, 'jumbotron')]//h1";
 
-    @task(10)
+    @task
     def index_page_with_regex_assertion(self):
         r = self.client.get("/")
         assert self.HOME_PAGE_TITLE_REGEX.search(r.text) is not None, \
             "Expected title has not been found!"
+
+    @task
+    def index_page_with_regex_assertion(self):
+        r = self.client.get("/")
+        tree = html.fromstring(r.text)
+        assert len(tree.xpath(self.HOME_PAGE_TITLE_XPATH)) == 1, \
+                     "Expected title has not been found!"
 
 class AwesomeUser(HttpLocust):
     task_set = BrowseDocumentation
