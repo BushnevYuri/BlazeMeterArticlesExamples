45064db1f24e7c61ebc97860ab83cc2791d58b2b
Yuri Bushnev <ybushnev@alpha-sense.com>
2017-11-05 18:31:09 +0200

    [Powerful assertions with locust] - css assertion example

--- Powerfull assertions with Locust/PowerfullAssertionsWithLocust.py
+++ Powerfull assertions with Locust/PowerfullAssertionsWithLocust.py
@@ -4,7 +4,8 @@ from lxml import html
 
 class BrowseDocumentation(TaskSet):
     HOME_PAGE_TITLE_REGEX = re.compile(".*Welcome to the Simple Travel Agency!.*")
-    HOME_PAGE_TITLE_XPATH = "//div[contains(@class, 'jumbotron')]//h1";
+    HOME_PAGE_TITLE_XPATH = "//h1[contains(text(),'Welcome to the Simple Travel Agency!')]";
+    HOME_PAGE_TITLE_CSS = ".jumbotron h1";
 
     @task
     def index_page_with_regex_assertion(self):
@@ -18,6 +19,15 @@ class BrowseDocumentation(TaskSet):
         tree = html.fromstring(r.text)
         assert len(tree.xpath(self.HOME_PAGE_TITLE_XPATH)) == 1, \
                      "Expected title has not been found!"
+
+    @task
+    def index_page_with_css_assertion(self):
+        r = self.client.get("/")
+        tree = html.fromstring(r.text)
+        assert len(tree.cssselect(self.HOME_PAGE_TITLE_CSS)) == 1, \
+            "Title element is not unique"
+        assert tree.cssselect(self.HOME_PAGE_TITLE_CSS)[0].text == "Welcome to the Simple Travel Agency!", \
+            "Expected title has not been found!"
 
 class AwesomeUser(HttpLocust):
     task_set = BrowseDocumentation
